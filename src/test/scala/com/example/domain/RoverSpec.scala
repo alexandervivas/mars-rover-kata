2e5e583276b7801fe92a2b83b2f023692274128f
Alexander Vivas <falexvr@gmail.com>
2023-08-14 00:10:24 +0200

    Creo clase para pruebas

---
+++ src/test/scala/com/example/domain/RoverSpec.scala
@@ -0,0 +1,10 @@
+package com.example.domain
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.mockito.MockitoSugar
+
+class RoverSpec extends AnyFunSuite with MockitoSugar {
+
+
+
+}
