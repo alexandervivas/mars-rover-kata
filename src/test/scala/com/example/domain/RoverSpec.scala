0caec5d91c972f6c3784729d2d9a5b21e36f9a9b
Alexander Vivas <falexvr@gmail.com>
2023-08-14 00:29:50 +0200

    Red phase

--- src/test/scala/com/example/domain/RoverSpec.scala
+++ src/test/scala/com/example/domain/RoverSpec.scala
@@ -1,13 +1,15 @@
 package com.example.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.mockito.MockitoSugar
 
 class RoverSpec extends AnyFunSuite with MockitoSugar {
 
-  test("Validar que un Rover se crea por defecto en el centro de la primera cara del planeta mirando en dirección norte") {
+  test("Validar que un Rover se crea por defecto en el centro del plano en dirección norte") {
 
-
+    assertThat(rover.position).isEqualTo((3, 3))
+    assertThat(rover.direction).isEqualTo('N')
 
   }
 
