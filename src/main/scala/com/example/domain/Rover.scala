267a0197e09ee73842d0eaaae869c725a7ca0cd8
Alexander Vivas <falexvr@gmail.com>
2023-08-14 00:45:40 +0200

    Added implementation class

---
+++ src/main/scala/com/example/domain/Rover.scala
@@ -0,0 +1,16 @@
+package com.example.domain
+
+case class Rover(abscissa: Seq[Int], ordinate: Seq[Int], coordinates: (Int, Int), direction: Char) {
+
+}
+object Rover {
+
+  def apply(abscissa: Seq[Int], ordinate: Seq[Int]): Rover =
+    Rover(
+      abscissa,
+      ordinate,
+      (abscissa((abscissa.size / 2).ceil.toInt), ordinate((ordinate.size / 2).ceil.toInt)),
+      'N'
+    )
+
+}
