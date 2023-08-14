d124c09d92dd7fe5e38ee1f1c02447e5c09de34d
Alexander Vivas <falexvr@gmail.com>
2023-08-14 09:40:29 +0200

    Validar que un Rover se puede mover hacia la derecha estando en dirección N: refactor phase

--- src/main/scala/com/example/domain/Point.scala
+++ src/main/scala/com/example/domain/Point.scala
@@ -1,3 +1,11 @@
 package com.example.domain
 
-case class Point(x: Int, y: Int)
+case class Point(x: Int, y: Int) {
+  def up: Point = copy(y = y - 1)
+
+  def down: Point = copy(y = y + 1)
+
+  def right: Point = copy(x = x + 1)
+
+  def left: Point = copy(x = x - 1)
+}
--- src/main/scala/com/example/domain/Rover.scala
+++ src/main/scala/com/example/domain/Rover.scala
@@ -12,19 +12,19 @@ case class Rover(abscissa: Seq[Int], ordinate: Seq[Int], coordinates: Point, dir
 
   def moveForward(): Rover = copy(
     coordinates = direction match {
-      case Direction.N => coordinates.copy(y = coordinates.y - 1)
-      case Direction.S => coordinates.copy(y = coordinates.y + 1)
-      case Direction.E => coordinates.copy(x = coordinates.x + 1)
-      case Direction.W => coordinates.copy(x = coordinates.x - 1)
+      case Direction.N => coordinates.up
+      case Direction.S => coordinates.down
+      case Direction.E => coordinates.right
+      case Direction.W => coordinates.left
     }
   )
 
   def moveBackward(): Rover = copy(
     coordinates = direction match {
-      case Direction.N => coordinates.copy(y = coordinates.y + 1)
-      case Direction.S => coordinates.copy(y = coordinates.y - 1)
-      case Direction.E => coordinates.copy(x = coordinates.x - 1)
-      case Direction.W => coordinates.copy(x = coordinates.x + 1)
+      case Direction.N => coordinates.down
+      case Direction.S => coordinates.up
+      case Direction.E => coordinates.left
+      case Direction.W => coordinates.right
     }
   )
 
