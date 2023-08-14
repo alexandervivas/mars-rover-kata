8f0689365405575c87bc6d2efec43ce802f76d87
Alexander Vivas <falexvr@gmail.com>
2023-08-14 16:52:07 +0200

    El mapa debe incluir dos caras para representar el recorrido del Rover en ambas caras de un planeta: refactor phase

--- src/main/scala/com/example/domain/Planet.scala
+++ src/main/scala/com/example/domain/Planet.scala
@@ -9,6 +9,6 @@ case class Planet(width: Int, height: Int) {
   def drawPoint(point: Point, char: Char = 'X'): Seq[Seq[Char]] =
     frontSide.updated(point.longitude - 1, frontSide(point.longitude - 1).updated(point.latitude - 1, char))
       .zip(backSide)
-      .map { case (value, value1) => value ++ Seq(':') ++ value1 }
+      .map { case (front, back) => front ++ Seq(':') ++ back }
 
 }
