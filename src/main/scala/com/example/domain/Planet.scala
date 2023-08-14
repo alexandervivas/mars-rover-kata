97611a59db1bad87a6b134888d42ea26d0b07bba
Alexander Vivas <falexvr@gmail.com>
2023-08-14 16:47:38 +0200

    El mapa debe incluir dos caras para representar el recorrido del Rover en ambas caras de un planeta: green phase

--- src/main/scala/com/example/domain/Planet.scala
+++ src/main/scala/com/example/domain/Planet.scala
@@ -3,9 +3,12 @@ package com.example.domain
 case class Planet(width: Int, height: Int) {
 
   private val emptySpot: Char = '.'
-  private lazy val map: Seq[Seq[Char]] = Seq.fill(height)(Seq.fill(width)(emptySpot))
+  private lazy val frontSide: Seq[Seq[Char]] = Seq.fill(height)(Seq.fill(width)(emptySpot))
+  private lazy val backSide: Seq[Seq[Char]] = Seq.fill(height)(Seq.fill(width)(emptySpot))
 
   def drawPoint(point: Point, char: Char = 'X'): Seq[Seq[Char]] =
-    map.updated(point.longitude - 1, map(point.longitude - 1).updated(point.latitude - 1, char))
+    frontSide.updated(point.longitude - 1, frontSide(point.longitude - 1).updated(point.latitude - 1, char))
+      .zip(backSide)
+      .map { case (value, value1) => value ++ Seq(':') ++ value1 }
 
 }
